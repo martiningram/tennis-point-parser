a3373c16460b9dae35eb90790f066620c1026929
Martin Ingram <ingramm@student.unimelb.edu.au>
2019-05-18 18:08:55 +1000

    Starting to take shape!

--- point_parser/parse.py
+++ point_parser/parse.py
@@ -1,5 +1,12 @@
-from typing import List, Dict, Callable, Tuple
+from typing import List, Dict, Callable, Tuple, Optional
 from dataclasses import dataclass
+
+
+@dataclass
+class CompletedSet:
+
+    player_scores: Dict[str, int]
+    tiebreak_score: Optional[Dict[str, int]]
 
 
 @dataclass
@@ -19,7 +26,7 @@ class MatchState:
     cur_game_score: Dict[str, int]
 
     sets_won: Dict[str, int]
-    past_sets: List[Dict[str, int]]
+    past_sets: List[CompletedSet]
 
     def reset_game_score(self):
 
@@ -40,16 +47,19 @@ class FormatFunctions:
     # TODO: The set win condition might vary by set -- the third bool is the
     # whether it's the final set.
     set_win_condition: Callable[[int, int, bool], bool]
-    match_over_fun: Callable[[MatchState], bool]
+    is_tiebreak_fun: Callable[[int, int, bool], bool]
+    tiebreak_over: Callable[[int, int, bool], bool]
+
+    match_over_fun: Callable[[int, int], bool]
     service_game_over: Callable[[int, int], bool]
 
     # Takes total games & first server and returner
-    cur_server: Callable[[int, str, str], Tuple[str, str]]
+    roles_at_game_start: Callable[[int, str, str], Tuple[str, str]]
 
     is_final_set: Callable[[int], bool]
-    is_tiebreak_fun: Callable[[int, int], bool]
-    tiebreak_over: Callable[[int, int], bool]
-    tiebreak_server: Callable[[MatchState], Tuple[str, str]]
+
+    # Takes total points & first server and returner
+    tiebreak_roles: Callable[[int, str, str], Tuple[str, str]]
 
 
 def player_wins_set(
@@ -59,8 +69,16 @@ def player_wins_set(
         format_functions: FormatFunctions) -> MatchState:
 
     match_state.sets_won[winning_player] += 1
+    match_state.past_sets.append(
+        CompletedSet(
+            player_scores=match_state.cur_set_score,
+            tiebreak_score=(match_state.cur_game_score if
+                            match_state.is_tiebreak else None)
+        ))
 
-    if not format_functions.match_over_fun(match_state):
+    if not format_functions.match_over_fun(
+            match_state.cur_set_score[winning_player],
+            match_state.cur_set_score[losing_player]):
         match_state.set_num += 1
         match_state.reset_set_score()
 
@@ -78,27 +96,31 @@ def player_wins_game(match_state: MatchState,
 
     games_winner += 1
 
-    match_state.cur_game_score[winning_player] += 1
-    match_state.reset_game_score()
+    match_state.cur_set_score[winning_player] += 1
     match_state.total_games_played += 1
 
     # Set server and returner
-    match_state.server, match_state.returner = format_functions.cur_server(
-        match_state.total_games_played, match_state.first_server,
-        match_state.first_returner)
+    match_state.server, match_state.returner = \
+        format_functions.roles_at_game_start(match_state.total_games_played,
+                                             match_state.first_server,
+                                             match_state.first_returner)
 
     is_final_set = format_functions.is_final_set(match_state.set_num)
 
     # Check whether player won the set
-    if format_functions.set_win_condition(games_winner, games_loser):
+    if format_functions.set_win_condition(
+            match_state.cur_set_score[winning_player],
+            match_state.cur_set_score[losing_player], is_final_set):
 
         match_state = player_wins_set(match_state, winning_player,
-                                      losing_player)
-
-        return match_state
+                                      losing_player, format_functions)
 
     match_state.is_tiebreak = format_functions.is_tiebreak_fun(
-        games_winner, games_loser)
+        match_state.cur_set_score[winning_player],
+        match_state.cur_set_score[losing_player],
+        is_final_set)
+
+    match_state.reset_game_score()
 
     return match_state
 
@@ -115,19 +137,26 @@ def advance_tiebreak(server_won: bool,
 
     if format_functions.tiebreak_over(
             match_state.cur_game_score[match_state.server],
-            match_state.cur_game_score[match_state.returner]):
+            match_state.cur_game_score[match_state.returner],
+            format_functions.is_final_set(match_state.set_num)):
 
         winning_player, losing_player = get_point_winner_loser(
             match_state.server, match_state.returner, server_won)
 
         match_state = player_wins_game(match_state, winning_player,
-                                       losing_player)
+                                       losing_player, format_functions)
 
         return match_state
 
+    total_points = sum(match_state.cur_game_score.values())
+    start_server, start_returner = format_functions.roles_at_game_start(
+        match_state.total_games_played, match_state.first_server,
+        match_state.first_returner)
+
     # Otherwise, adjust who's serving
     match_state.server, match_state.returner = \
-        format_functions.tiebreak_server(match_state)
+        format_functions.tiebreak_roles(total_points, start_server,
+                                        start_returner)
 
     return match_state
 
@@ -135,8 +164,7 @@ def advance_tiebreak(server_won: bool,
 def get_point_winner_loser(server, returner, server_won):
 
     winning_player, losing_player = (
-        (match_state.server, match_state.returner) if server_won
-        else (match_state.returner, match_state.server))
+        (server, returner) if server_won else (returner, server))
 
     return winning_player, losing_player
 
@@ -161,7 +189,9 @@ def advance_service_game(server_won: bool,
             match_state.server, match_state.returner, server_won)
 
         match_state = player_wins_game(match_state, winning_player,
-                                       losing_player)
+                                       losing_player, format_functions)
+
+    return match_state
 
 
 def process_win_loss_vector(win_loss_vector: List[bool],
@@ -181,6 +211,8 @@ def process_win_loss_vector(win_loss_vector: List[bool],
         match_state = advance_service_game(cur_win_loss, match_state,
                                            format_functions)
 
+    print(match_summary_string(match_state))
+
     # Recurse
     return process_win_loss_vector(win_loss_vector[1:], match_state,
                                    format_functions)
@@ -196,9 +228,69 @@ def create_start_match_state(first_server: str, first_returner: str,
         first_server=first_server,
         first_returner=first_returner,
         set_num=0,
-        total_games_player=0,
+        total_games_played=0,
         cur_set_score={first_server: 0, first_returner: 0},
         cur_game_score={first_server: 0, first_returner: 0},
         sets_won={first_server: 0, first_returner: 0},
         past_sets=list()
     )
+
+
+def transform_to_score_format(points_p1: int, points_p2: int) -> str:
+
+    lookup = {
+        0: '0',
+        1: '15',
+        2: '30',
+        3: '40',
+        4: 'AD',
+    }
+
+    if max(points_p1, points_p2) > 3:
+
+        difference = points_p1 - points_p2
+        assert abs(difference) <= 1
+
+        if difference == 1:
+            points_p1, points_p2 = 4, 3
+        elif difference == 0:
+            points_p1, points_p2 = 3, 3
+        elif difference == -1:
+            points_p1, points_p2 = 3, 4
+
+    return f'{lookup[points_p1]}:{lookup[points_p2]}'
+
+
+def match_summary_string(match_state: MatchState) -> str:
+
+    cur_server, cur_returner = match_state.server, match_state.returner
+
+    string = f'{cur_server} - {cur_returner}: '
+
+    for cur_set in match_state.past_sets:
+
+        to_add = (f'{cur_set.player_scores[cur_server]}-'
+                  f'{cur_set.player_scores[cur_returner]} ')
+
+        if cur_set.tiebreak_score is not None:
+
+            min_score = min(cur_set.tiebreak_score.values())
+
+        to_add += f'({min_score}) '
+
+        string += to_add
+
+    # Add current set:
+    string += (f'{match_state.cur_set_score[cur_server]}-'
+               f'{match_state.cur_set_score[cur_returner]} ')
+
+    # Add current game score
+    if match_state.is_tiebreak:
+        string += (f'{match_state.cur_game_score[cur_server]}:'
+                   f'{match_state.cur_game_score[cur_returner]}')
+    else:
+        string += transform_to_score_format(
+            match_state.cur_game_score[cur_server],
+            match_state.cur_game_score[cur_returner])
+
+    return string
--- point_parser/standard_win_functions.py
+++ point_parser/standard_win_functions.py
@@ -1,10 +1,10 @@
-import numpy as np
+from typing import Tuple
 
 
 def set_win_condition_ad_set(games_p1: int, games_p2: int) -> bool:
 
     enough_games = max(games_p1, games_p2) >= 6
-    enough_margin = np.abs(games_p1 - games_p2) >= 2
+    enough_margin = abs(games_p1 - games_p2) >= 2
 
     return enough_games and enough_margin
 
@@ -24,3 +24,79 @@ def set_win_condition_ad_final_set(games_p1: int, games_p2: int,
         return set_win_condition_ad_set(games_p1, games_p2)
     else:
         return set_win_condition_tb_set(games_p1, games_p2)
+
+
+def match_over_best_of(sets_won_p1: int, sets_won_p2: int,
+                       best_of: int) -> bool:
+
+    assert best_of in [3, 5]
+
+    max_sets = max(sets_won_p1, sets_won_p2)
+
+    if best_of == 3:
+        return max_sets == 2
+
+    else:
+        return max_sets == 3
+
+
+def standard_service_game_over(points_p1: int, points_p2: int,
+                               has_ad: bool = True) -> bool:
+
+    diff = abs(points_p1 - points_p2)
+    max_pts = max(points_p1, points_p2)
+
+    enough_points = max_pts >= 4
+    enough_diff = diff >= 2
+
+    if has_ad:
+        return enough_points and enough_diff
+    else:
+        return enough_points
+
+
+def roles_at_game_start(total_games_played: int, first_server: str,
+                        first_returner: str) -> Tuple[str, str]:
+
+    modulo = total_games_played % 2
+
+    # If 1 game played, returner plays.
+    if modulo == 1:
+        return first_returner, first_server
+    else:
+        return first_server, first_returner
+
+
+def is_tiebreak_fun_uso_style(games_p1: int, games_p2: int,
+                              is_final_set: bool) -> bool:
+
+    return games_p1 == 6 and games_p2 == 6
+
+
+def is_tiebreak_fun_no_tb_final_set(games_p1: int, games_p2: int,
+                                    is_final_set: bool) -> bool:
+
+    if is_final_set:
+        return False
+    else:
+        return is_tiebreak_fun_uso_style(games_p1, games_p2, is_final_set)
+
+
+def tiebreak_over_standard(points_p1: int, points_p2: int) -> bool:
+
+    enough_points = max(points_p1, points_p2) >= 7
+    enough_diff = abs(points_p1 - points_p2) >= 2
+
+    return enough_points and enough_diff
+
+
+def tiebreak_roles_standard(total_points: int, first_server: str,
+                            first_returner: str) -> Tuple[str, str]:
+
+    if total_points % 4 in [1, 2]:
+
+        return (first_returner, first_server)
+
+    else:
+
+        return (first_server, first_returner)
